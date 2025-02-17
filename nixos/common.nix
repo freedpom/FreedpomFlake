ebc39c967ec48a0bc81cc821b8c6d6750a77c64c
Neochaotics <neochaotics@pm.me>
2025-02-17 00:58:54 -0500

    refactor

---
+++ nixos/common.nix
@@ -0,0 +1,66 @@
+{
+  pkgs,
+  lib,
+  config,
+  modulesPath,
+  hostname,
+  inputs,
+  ...
+}:
+let
+  cfg = config.ff.common;
+in
+{
+  options.ff.common = {
+    enable = lib.mkEnableOption "Enable nix system configuration";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.gpm.enable = true;
+
+    fonts.packages =
+      with pkgs;
+      [
+        noto-fonts
+        liberation_ttf
+
+      ]
+      ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+    networking = {
+      hostName = hostname; # Define your hostname.
+      hostId = "00000000"; # Define your host ID.
+      networkmanager.enable = true;
+    };
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+
+    time.timeZone = "America/New_York";
+
+    console = {
+      earlySetup = true;
+      #font = "Lat2-Terminus16";
+      useXkbConfig = true;
+    };
+
+    imports = [ (modulesPath + "/profiles/minimal.nix") ];
+    boot.initrd.includeDefaultModules = false;
+    disabledModules = [
+      (modulesPath + "/profiles/all-hardware.nix")
+      (modulesPath + "/profiles/base.nix")
+    ];
+
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      backupFileExtension = "bk";
+      extraSpecialArgs = {
+        inherit inputs;
+      };
+    };
+  };
+}
