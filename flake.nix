ffdda18db2ee495a1adc5678eff1513af88858b2
yung-gimp <>
2025-03-02 22:52:39 -0500

    FreedpomFormatter

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   description = "Collection of NixOS and Home-Manager presets";
 
   inputs = {
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    devshell.url = "github:numtide/devshell";
-    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+
+    fpFmt = {
+      url = "github:freedpom/FreedpomFormatter";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Flake infrastructure
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -24,66 +26,10 @@
         "aarch64-linux"
       ];
       imports = [
-        inputs.treefmt-nix.flakeModule
-        inputs.devshell.flakeModule
-        inputs.flake-root.flakeModule
-        inputs.git-hooks-nix.flakeModule
         inputs.home-manager.flakeModules.home-manager
+        inputs.fpFmt.flakeModules.default
       ];
 
-      # Per-system configuration
-      perSystem =
-        { config, pkgs, ... }:
-        {
-          # Code formatting and linting setup
-          treefmt.config = {
-            inherit (config.flake-root) projectRootFile;
-            flakeCheck = false;
-            programs = {
-              # Nix formatting tools
-              nixfmt = {
-                enable = true;
-                package = pkgs.nixfmt-rfc-style;
-              };
-              statix.enable = true; # Static analysis for Nix
-              deadnix.enable = true; # Detect dead code in Nix
-
-              typos.enable = true;
-              typos.excludes = [
-                "*.png"
-                "*.yaml"
-              ];
-
-              # Additional formatters
-              actionlint.enable = true; # GitHub Actions linter
-              mdformat.enable = true; # Markdown formatter
-              yamlfmt.enable = true;
-              shfmt.enable = true;
-            };
-          };
-
-          # Development environment configuration
-          devshells.default = {
-            name = "nixdev";
-            motd = ""; # Message of the day
-            packages = [
-              pkgs.nil # Nix Language Server
-              config.treefmt.build.wrapper
-            ] ++ (pkgs.lib.attrValues config.treefmt.build.programs);
-          };
-
-          # Git pre-commit hooks
-          pre-commit.settings.hooks = {
-            treefmt = {
-              enable = true;
-              package = config.treefmt.build.wrapper;
-            };
-            statix = {
-              enable = true;
-              package = config.treefmt.build.programs.statix;
-            };
-          };
-        };
       flake = {
         nixosModules = {
           freedpomFlake = ./nixos;
