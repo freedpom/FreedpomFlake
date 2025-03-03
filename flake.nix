0d4bc2abb4e8089b24dc9e84cb939ac7e26364d3
Neochaotics <neochaotics@pm.me>
2025-03-03 15:28:58 -0500

    refactor

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
-  description = "Collection of NixOS and Home-Manager presets";
+  description = "NixOS and Home-Manager presets";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     fpFmt = {
       url = "github:freedpom/FreedpomFormatter";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Flake infrastructure
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-root.url = "github:srid/flake-root";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
   };
 
   outputs =
@@ -25,6 +25,7 @@
         "x86_64-linux"
         "aarch64-linux"
       ];
+
       imports = [
         inputs.home-manager.flakeModules.home-manager
         inputs.fpFmt.flakeModule
