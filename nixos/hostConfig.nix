b0950af43efe862c5e99d112746ead0a24ec345c
yung-gimp <>
2025-03-10 16:49:18 -0400

    feat: change display definitions

--- nixos/hostConfig.nix
+++ nixos/hostConfig.nix
@@ -1,38 +1,42 @@
 { lib, ... }:
 {
-
   options.hostConf = {
-    displayType = lib.mkOption {
-      type = lib.types.enum [
-        "headless"
-        "kmscon"
-        "wayland"
-      ];
-      default = "headless";
-      example = "wayland";
-      description = "Configure the system for a headless server, a KMS console, or a Wayland desktop environment.";
+    displayType = {
+      headless = lib.mkEnableOption;
+      wayland = lib.mkEnableOption;
+      kmscon = lib.mkOption {
+        type = lib.types.list;
+        default = "";
+        description = "list of ttys that will be enabled with kms console";
+      };
     };
-    tags = lib.mkOption {
-      type = lib.types.listOf lib.types.enum [
-        "power-save" # Laptops & small arm devices
-        "gaming" # High performance gaming
-        "rt-audio" # Real-time audio
-      ];
+    agetty = lib.mkOption {
+      type = lib.types.list;
       default = "";
-      example = "gaming rt-audio";
-      description = "";
-    };
-    inputType = lib.mkOption {
-      type = lib.types.enum [
-        "controller"
-        "keyboard"
-        "mouse"
-        "touch"
-        "trackpad"
-      ];
-      default = "keyboard";
-      example = "mouse";
-      description = "Configure the system UI for input devices such as a keyboard, mouse, touch screen, trackpad, or controller.";
+      description = "list of ttys that will use the default console";
     };
   };
+  tags = lib.mkOption {
+    type = lib.types.listOf lib.types.enum [
+      "power-save" # Laptops & small arm devices
+      "gaming" # High performance gaming
+      "rt-audio" # Real-time audio
+    ];
+    default = "";
+    example = "gaming rt-audio";
+    description = "";
+  };
+  inputType = lib.mkOption {
+    type = lib.types.enum [
+      "controller"
+      "keyboard"
+      "mouse"
+      "touch"
+      "trackpad"
+    ];
+    default = "keyboard";
+    example = "mouse";
+    description = "Configure the system UI for input devices such as a keyboard, mouse, touch screen, trackpad, or controller.";
+  };
+
 }
