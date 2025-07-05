8f8543864a04d4c42657026f0df0e422895fd70a
Neochaotics <neochaotics@pm.me>
2025-07-05 16:51:21 -0400

    feat: performance

---
+++ nixos/system/performance.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.ff.system.performance;
+in
+{
+  options.ff.system.performance = {
+    enable = lib.mkEnableOption "Enable performance tweaks ";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.irqbalance.enable = true;
+    systemd = {
+      watchdog = {
+        runtimeTime = "30s";
+        rebootTime = "45s";
+      };
+      coredump.enable = false;
+    };
+
+  };
+}
