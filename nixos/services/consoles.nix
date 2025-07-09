03347d2407c55e6c075027de97a091ecd65863d1
yung-gimp <>
2025-07-09 17:52:47 -0400

    idk if submodule better

--- nixos/services/consoles.nix
+++ nixos/services/consoles.nix
@@ -1,7 +1,3 @@
-# Console configuration module for KMS console
-# KNOWN ISSUE: enabling kmscon@tty1 causes it to take over ttys,
-# possibly due to seat configuration issues
-
 {
   config,
   lib,
@@ -12,22 +8,10 @@ let
 
   cfg = config.ff.services.kmscon;
 
-  baseArgs =
-    [
-      "--login-program"
-      "${pkgs.shadow}/bin/login"
-    ]
-    ++ lib.optionals (cfg.autologinUser != null) [
-      "--autologin"
-      cfg.autologinUser
-    ];
-
-  gettyCmd = args: "${lib.getExe' pkgs.util-linux "agetty"} ${lib.escapeShellArgs baseArgs} ${args}";
-
 in
 {
   # Configuration options for KMS console
-  options.ff.services.kmscon = {
+  options.ff.services.consoles = {
     # Core options
     enable = lib.mkEnableOption "Enable kms console";
 
@@ -35,20 +19,14 @@ in
     autologinUser = lib.mkOption {
       type = lib.types.nullOr lib.types.str;
       default = null;
-      description = ''
-        Username of the account that will be automatically logged in at the console.
-        If unspecified, a login prompt is shown as usual.
-      '';
+      description = "Default autologin user for all consoles";
     };
 
     # TTY configuration
-    disableAt = lib.mkOption {
-      type = lib.types.nullOr (lib.types.listOf lib.types.str);
-      default = null;
-      description = "Numeric identifier of ttys that should not use the kms console, must include tty1 or tty switching will not work";
-      example = [
-        "tty1"
-      ];
+     kmscon = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.subModule {
+
+      })
     };
 
   };
