d84c30126f08d4eebf33635f6f40b08fc1ffacb5
yung-gimp <172232649+yung-gimp@users.noreply.github.com>
2025-07-26 18:28:40 -0400

    it grows

--- nixos/services/consoles.nix
+++ nixos/services/consoles.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -34,9 +35,9 @@ in
       default = false;
       description = "Configure getty to run on specific ttys or all available ttys";
       example = ''
+        [ "user@tty1" "tty3" "tty4" ] - autologin on user@tty#, run normally on other specified ttys
         true - run on all ttys not taken by other consoles, provides 2 consoles by default or fills to highest number used
         false - don't run at all
-        [ "user@tty1" "tty3" "tty4" ] - autologin on user@tty#, run normally on other specified ttys
       '';
     };
 
@@ -55,14 +56,43 @@ in
       default = false;
       description = "Configure kmscon to run on specific ttys or all available ttys";
       example = ''
-        true - run on all ttys not taken by other consoles, provides 2 consoles by default or fills to highest number used
-        false - don't run at all
         [ "user@tty1" "tty3" "tty4" ] - autologin on user@tty#, run normally on other specified ttys
+        true - run on all ttys not taken by other consoles, provides 2 consoles by default
+        false - don't run at all
       '';
     };
 
   };
-  config = lib.mkIf cfg.getty != false || cfg.mkscon != false {
+  config = let
+    mergedIds = if lib.isList cfg.getty && cfg.kmscon then cfg.getty ++ cfg.kmscon else if lib.isList cfg.getty then cfg.getty else if lib.isList cfg.kmscon then cfg.kmscon else throw "you broke it :("; # merge ttyids into a single list
+
+    ttyIds = (ids: lib.flatten (lib.map (id: lib.strings.match ".*([t]+[t]+[y].*)" id) ids)); # filter tty identifiers from a list
+
+    largeId = (ids: lib.foldl' (m: c: if c > m then c else m) (builtins.head (ttyIds v)) (builtins.tail (ttyIds ids))); # return highest tty identifier
+
+    autologinAt = lib.filter (id: lib.strings.hasInfix "@" id) mergedIds; # filter for user@tty#
+
+    userFilter = id: lib.strings.match "(.*)[@]" id;
+
+    gettyUnit = id: {
+      serviceConfig.ExecStart = [
+        ""
+        "${lib.getExe' pkgs.util-linux "agetty"} --login-program ${pkgs.shadow}/bin/login ${lib.optionals (lib.elem id autologinAt) "--autologin ${userFilter id}"}"
+      ];
+    };
+  in 
+  lib.mkIf cfg.getty != false || cfg.kmscon != false {
     console.enable = false; # Disable default console creation
+    systemd.services = lib.genAttrs gettyAt gettyUnit // lib.genAttrs kmsAt kmsUnit;
+    assertions = [
+      {
+        assertion = !(cfg.getty && cfg.kmscon);
+        message = "Getty and kmscon cannot both be true";
+      }
+      {
+        assertion = cfg.getty != [] && cfg.kmscon != [];
+        message = "Neither getty nor kmscon can be set to an empty list";
+      }
+    ];
   };
 }
