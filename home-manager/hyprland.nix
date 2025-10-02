af1b517eff8ee99c2542984d1941498373eecb6a
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-10-02 02:05:14 -0400

    fix: i knew this would happen haha

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -16,40 +16,38 @@
       )
     )
     {
-      wayland.windowManager.hyprland.settings.monitor =
-        lib.mkIf (config.wayland.windowManager.hyprland.settings.monitor == null)
-        (
-          lib.mapAttrsToList (
-            name: cfg: let
-              opt = attr: pred: prefix: suffix:
-                if (cfg ? attr) && pred cfg.${attr}
-                then "${prefix}${toString cfg.${attr}}${suffix}"
-                else "";
+      wayland.windowManager.hyprland.settings.monitor = lib.mkDefault (
+        lib.mapAttrsToList (
+          name: cfg: let
+            opt = attr: pred: prefix: suffix:
+              if (cfg ? ${attr}) && pred cfg.${attr}
+              then "${prefix}${toString cfg.${attr}}${suffix}"
+              else "";
 
-              resolutionStr = "${toString cfg.resolution.width}x${toString cfg.resolution.height}@${toString cfg.framerate}";
-              positionStr = cfg.position;
-              scaleStr = toString cfg.scale;
+            resolutionStr = "${toString cfg.resolution.width}x${toString cfg.resolution.height}@${toString cfg.framerate}";
+            positionStr = cfg.position;
+            scaleStr = toString cfg.scale;
 
-              base = "${name}, ${resolutionStr}, ${positionStr}, ${scaleStr}";
+            base = "${name}, ${resolutionStr}, ${positionStr}, ${scaleStr}";
 
-              transformStr = opt "transform" (x: x != null && x != 0) ", transform, " "";
-              mirrorStr = opt "mirror" (x: x != null) ", mirror, " "";
-              bitdepthStr = opt "colorDepth" (x: x != 24) ", bitdepth, " "";
-              colorProfileStr = opt "colorProfile" (x: x != null) ", cm, " "";
-              sdrBrightnessStr = opt "sdrBrightness" (x: x != null) ", sdrbrightness, " "";
-              sdrSaturationStr = opt "sdrSaturation" (x: x != null) ", sdrsaturation, " "";
-              vrrStr = opt "vrr" (x: x != 0) ", vrr, " "";
-            in
-              base
-              + transformStr
-              + mirrorStr
-              + bitdepthStr
-              + colorProfileStr
-              + sdrBrightnessStr
-              + sdrSaturationStr
-              + vrrStr
-          ) (lib.mkForce (lib.attrByPath ["ff" "hardware" "displays"] {} osConfig))
-        );
+            transformStr = opt "transform" (x: x != null && x != 0) ", transform, " "";
+            mirrorStr = opt "mirror" (x: x != null) ", mirror, " "";
+            bitdepthStr = opt "colorDepth" (x: x != 24) ", bitdepth, " "";
+            colorProfileStr = opt "colorProfile" (x: x != null) ", cm, " "";
+            sdrBrightnessStr = opt "sdrBrightness" (x: x != null) ", sdrbrightness, " "";
+            sdrSaturationStr = opt "sdrSaturation" (x: x != null) ", sdrsaturation, " "";
+            vrrStr = opt "vrr" (x: x != 0) ", vrr, " "";
+          in
+            base
+            + transformStr
+            + mirrorStr
+            + bitdepthStr
+            + colorProfileStr
+            + sdrBrightnessStr
+            + sdrSaturationStr
+            + vrrStr
+        ) (lib.mkForce (lib.attrByPath ["ff" "hardware" "displays"] {} osConfig))
+      );
 
       wayland.windowManager.hyprland.settings.workspace = lib.concatLists (
         lib.mapAttrsToList (name: cfg: map (ws: "${ws}, monitor:${name}") cfg.workspaces) (
