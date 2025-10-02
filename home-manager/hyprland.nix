751c9b3784720453057e14d10447506f537c10c6
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-10-02 02:13:06 -0400

    fix: aaaaaaa

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -15,44 +15,55 @@
         > 0
       )
     )
-    {
-      wayland.windowManager.hyprland.settings = lib.mkDefault {
-        monitor = lib.mapAttrsToList (
-          name: cfg: let
-            opt = attr: pred: prefix: suffix:
-              if (cfg ? ${attr}) && pred cfg.${attr}
-              then "${prefix}${toString cfg.${attr}}${suffix}"
-              else "";
+    (
+      let
+        displays = lib.mkForce (lib.attrByPath ["ff" "hardware" "displays"] {} osConfig);
 
-            resolutionStr = "${toString cfg.resolution.width}x${toString cfg.resolution.height}@${toString cfg.framerate}";
-            positionStr = cfg.position;
-            scaleStr = toString cfg.scale;
+        # Should we set monitor?
+        setMonitor = !(config.wayland.windowManager.hyprland.settings ? monitor);
+        setWorkspace = !(config.wayland.windowManager.hyprland.settings ? workspace);
 
-            base = "${name}, ${resolutionStr}, ${positionStr}, ${scaleStr}";
+        monitorSettings =
+          lib.mapAttrsToList (
+            name: cfg: let
+              opt = attr: pred: prefix: suffix:
+                if (cfg ? ${attr}) && pred cfg.${attr}
+                then "${prefix}${toString cfg.${attr}}${suffix}"
+                else "";
 
-            transformStr = opt "transform" (x: x != null && x != 0) ", transform, " "";
-            mirrorStr = opt "mirror" (x: x != null) ", mirror, " "";
-            bitdepthStr = opt "colorDepth" (x: x != 24) ", bitdepth, " "";
-            colorProfileStr = opt "colorProfile" (x: x != null) ", cm, " "";
-            sdrBrightnessStr = opt "sdrBrightness" (x: x != null) ", sdrbrightness, " "";
-            sdrSaturationStr = opt "sdrSaturation" (x: x != null) ", sdrsaturation, " "";
-            vrrStr = opt "vrr" (x: x != 0) ", vrr, " "";
-          in
-            base
-            + transformStr
-            + mirrorStr
-            + bitdepthStr
-            + colorProfileStr
-            + sdrBrightnessStr
-            + sdrSaturationStr
-            + vrrStr
-        ) (lib.mkForce (lib.attrByPath ["ff" "hardware" "displays"] {} osConfig));
+              resolutionStr = "${toString cfg.resolution.width}x${toString cfg.resolution.height}@${toString cfg.framerate}";
+              positionStr = cfg.position;
+              scaleStr = toString cfg.scale;
 
-        workspace = lib.concatLists (
-          lib.mapAttrsToList (name: cfg: map (ws: "${ws}, monitor:${name}") cfg.workspaces) (
-            lib.mkForce (lib.attrByPath ["ff" "hardware" "displays"] {} osConfig)
+              base = "${name}, ${resolutionStr}, ${positionStr}, ${scaleStr}";
+
+              transformStr = opt "transform" (x: x != null && x != 0) ", transform, " "";
+              mirrorStr = opt "mirror" (x: x != null) ", mirror, " "";
+              bitdepthStr = opt "colorDepth" (x: x != 24) ", bitdepth, " "";
+              colorProfileStr = opt "colorProfile" (x: x != null) ", cm, " "";
+              sdrBrightnessStr = opt "sdrBrightness" (x: x != null) ", sdrbrightness, " "";
+              sdrSaturationStr = opt "sdrSaturation" (x: x != null) ", sdrsaturation, " "";
+              vrrStr = opt "vrr" (x: x != 0) ", vrr, " "";
+            in
+              base
+              + transformStr
+              + mirrorStr
+              + bitdepthStr
+              + colorProfileStr
+              + sdrBrightnessStr
+              + sdrSaturationStr
+              + vrrStr
           )
+          displays;
+
+        workspaceSettings = lib.concatLists (
+          lib.mapAttrsToList (name: cfg: map (ws: "${ws}, monitor:${name}") cfg.workspaces) displays
         );
-      };
-    };
+      in {
+        wayland.windowManager.hyprland.settings = {
+          monitor = lib.mkIf setMonitor monitorSettings;
+          workspace = lib.mkIf setWorkspace workspaceSettings;
+        };
+      }
+    );
 }
