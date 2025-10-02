55f2dc8bd1660d11af16d5be1edf0f26534b593f
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-10-02 01:19:55 -0400

    fix: why no workie

--- nixos/hardware/default.nix
+++ nixos/hardware/default.nix
@@ -1,3 +1,5 @@
-{lib, ...}: {
-  imports = lib.map (n: ./. + /${n}) (lib.attrNames (lib.attrsets.filterAttrs (n: v: (((v == "directory") && (lib.hasAttr "default.nix" (builtins.readDir ./${n}))) || (lib.hasSuffix ".nix" n) && (n != "default.nix"))) (builtins.readDir ./.)));
+{...}: {
+  imports = [
+    ./displays.nix
+  ];
 }
