9e03c04b81e5aa96cc31f4171c30d40b0c7c9ad9
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-11-12 00:08:20 -0500

    fix: i just want to build my config without errors

--- nixos/system/home-manager/default.nix
+++ nixos/system/home-manager/default.nix
@@ -2,11 +2,12 @@
   inputs,
   lib,
   ...
-}: {
+}:
+{
   options.ff.system.home-manager.enable = lib.mkOption {
     type = lib.types.bool;
     default = true;
     description = "Default system configs for home-manager, can be disabled.";
   };
-  imports = lib.optionals (inputs ? home-manager) [./home-manager.nix];
+  #imports = lib.optionals (inputs ? home-manager) [./home-manager.nix];
 }
