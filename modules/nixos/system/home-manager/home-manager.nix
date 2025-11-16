f0ee3ae8e3fe415556f592e4ff6c9747a2f723f0
yung-gimp <172232649+yung-gimp@users.noreply.github.com>
2025-11-16 15:24:58 -0500

    fix: the boolean
    dont write modules right before bed

--- modules/nixos/system/home-manager/home-manager.nix
+++ modules/nixos/system/home-manager/home-manager.nix
@@ -6,16 +6,14 @@
 }:
 {
   # Allow disabling via the enable option, only enables if home-manager module is actually imported
-  config =
-    lib.mkIf config.ff.system.home-manager.enable
-    && (config ? home-manager) {
-      home-manager = {
-        backupFileExtension = "bk";
-        extraSpecialArgs = {
-          inherit inputs;
-        };
-        useGlobalPkgs = true;
-        useUserPackages = true;
+  config = lib.mkIf (config.ff.system.home-manager.enable && config ? home-manager) {
+    home-manager = {
+      backupFileExtension = "bk";
+      extraSpecialArgs = {
+        inherit inputs;
       };
+      useGlobalPkgs = true;
+      useUserPackages = true;
     };
+  };
 }
