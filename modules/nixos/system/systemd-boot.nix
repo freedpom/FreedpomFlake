d3859a9290694cac809df3b180284686bf7c0b53
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-11-16 20:23:05 -0500

    feat: tidy up systemd-boo

--- modules/nixos/system/systemd-boot.nix
+++ modules/nixos/system/systemd-boot.nix
@@ -19,25 +19,26 @@ in
       loader = {
         timeout = lib.mkDefault 0;
         systemd-boot = {
-          editor = false;
+          editor = lib.mkForce false;
+          consoleMode = "auto";
           enable = true;
           graceful = lib.mkDefault true;
           configurationLimit = lib.mkDefault 10;
         };
         efi = {
           efiSysMountPoint = "/boot";
-          canTouchEfiVariables = lib.mkDefault false;
+          canTouchEfiVariables = true;
         };
       };
       kernelParams = [
         "quiet"
-        "splash"
-        "boot.shell_on_fail"
         "loglevel=3"
-        "systemd.show_status=false"
         "rd.udev.log_level=3"
         "udev.log_priority=3"
-        "vga=current"
+        "splash"
+        "boot.shell_on_fail"
+        "systemd.show_status=auto"
+        # "vga=current"
       ];
       consoleLogLevel = lib.mkForce 0;
       initrd = {
