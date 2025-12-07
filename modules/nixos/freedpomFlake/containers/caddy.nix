6bb7f871c4e2ef18b9b5403456f3e923c1c9019f
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2025-12-07 00:12:12 -0500

    feat: contain

---
+++ modules/nixos/freedpomFlake/containers/caddy.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.ff.containers.caddy;
+in
+{
+  options.ff.containers.caddy = {
+    enable = lib.mkEnableOption "Enable";
+  };
+  config = lib.mkIf cfg.enable {
+    virtualisation.oci-containers.containers = {
+      caddy = {
+        image = "caddy:2.10.2";
+        autoStart = true;
+      };
+    };
+  };
+}
