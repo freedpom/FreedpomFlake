5179dffb051f2d612e8f9a18c7242c400e4812f9
yung-gimp <172232649+yung-gimp@users.noreply.github.com>
2025-12-07 13:20:32 -0500

    fix: boot.blacklistedKernelModules

--- modules/nixos/freedpomFlake/system/boot.nix
+++ modules/nixos/freedpomFlake/system/boot.nix
@@ -94,36 +94,37 @@ in
           graceful = lib.mkDefault true;
           configurationLimit = cfg.loaderEntries;
         };
-        blacklistedKernelModules = [
-          # Obscure network protocols
-          "ax25"
-          "netrom"
-          "rose"
-
-          # Old or rare or insufficiently audited filesystems
-          "adfs"
-          "affs"
-          "bfs"
-          "befs"
-          "cramfs"
-          "efs"
-          "erofs"
-          "exofs"
-          "freevxfs"
-          "f2fs"
-          "hfs"
-          "hpfs"
-          "jfs"
-          "minix"
-          "nilfs2"
-          "ntfs"
-          "omfs"
-          "qnx4"
-          "qnx6"
-          "sysv"
-          "ufs"
-        ];
       };
+
+      blacklistedKernelModules = [
+        # Obscure network protocols
+        "ax25"
+        "netrom"
+        "rose"
+
+        # Old or rare or insufficiently audited filesystems
+        "adfs"
+        "affs"
+        "bfs"
+        "befs"
+        "cramfs"
+        "efs"
+        "erofs"
+        "exofs"
+        "freevxfs"
+        "f2fs"
+        "hfs"
+        "hpfs"
+        "jfs"
+        "minix"
+        "nilfs2"
+        "ntfs"
+        "omfs"
+        "qnx4"
+        "qnx6"
+        "sysv"
+        "ufs"
+      ];
 
       kernelParams =
         (lib.optionals (cfg.verbosity == "quiet") [
