5885312509849b114f908ff4b07b67ef63000dee
yung-gimp <172232649+yung-gimp@users.noreply.github.com>
2026-01-05 16:19:45 -0500

    still broke

--- modules/flake/flib.nix
+++ modules/flake/flib.nix
@@ -3,13 +3,17 @@
   options.flake = inputs.flake-parts.lib.mkSubmoduleOptions {
     flib = lib.mkOption {
       type = lib.types.attrsOf lib.types.anything;
-      default = { };
       description = "flib helper functions";
     };
   };
   config.flake.flib = rec {
     mkHosts =
       dir:
+      {
+        modules ? [ ],
+        specialArgs ? { },
+        ...
+      }:
       let
         hostnames = lib.attrNames (
           lib.filterAttrs (n: v: v == "directory" && (builtins.readDir "${dir}/${n}") ? "default.nix") (
@@ -20,33 +24,10 @@
       lib.genAttrs hostnames (
         hostname:
         inputs.nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit
-              inputs
-              hostname
-              ;
+          modules = modules ++ [ "${dir}/${hostname}" ];
+          specialArgs = specialArgs // {
+            inherit hostname;
           };
-          modules = [ "${dir}/${hostname}" ];
-        }
-      );
-    mkHostsWithModules = dir: myModules: {inputs, ...}:
-      let
-        hostnames = lib.attrNames (
-          lib.filterAttrs (n: v: v == "directory" && (builtins.readDir "${dir}/${n}") ? "default.nix") (
-            builtins.readDir dir
-          )
-        );
-      in
-      lib.genAttrs hostnames (
-        hostname:
-        inputs.nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit
-              # inputs
-              hostname
-              ;
-          };
-          modules = [ "${dir}/${hostname}" ] ++ myModules;
         }
       );
   };
