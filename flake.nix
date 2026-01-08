6447151ecb339a0790d976e20e91958f20106001
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2026-01-08 17:01:47 -0500

    feat: add forgecode

--- flake.nix
+++ flake.nix
@@ -10,13 +10,17 @@
     wm-hypr.url = "github:hyprwm/Hyprland?ref=v0.53.1";
     wm-niri.url = "github:sodiboo/niri-flake";
     home-manager.url = "github:nix-community/home-manager";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } (
       let
-        fmtModule = flake-parts.lib.importApply ./modules/flake/fmt-module.nix { inherit inputs; };
+        fmtModule = flake-parts.lib.importApply ./modules/flake/formatModule.nix { inherit inputs; };
       in
       {
         systems = [
@@ -27,11 +31,13 @@
         imports = [
           fmtModule
           inputs.home-manager.flakeModules.home-manager
+          inputs.flake-parts.flakeModules.easyOverlay
+          inputs.flake-parts.flakeModules.partitions
           ./modules/home-manager
           ./modules/nixos
           ./packages
           ./modules/flake/windowManagers
-          ./modules/flake/core
+          ./modules/flake/coreModule
         ];
         flake = {
           inherit fmtModule;
--- packages/default.nix
+++ packages/default.nix
@@ -1,11 +1,17 @@
 {
   perSystem =
-    { pkgs, inputs', ... }:
     {
-      packages.container-hello = inputs'.nix2container.packages.nix2container.buildImage {
-        name = "hello";
-        config = {
-          entrypoint = [ "${pkgs.hello}/bin/hello" ];
+      pkgs,
+      inputs',
+      ...
+    }:
+    {
+      packages = {
+        container-hello = inputs'.nix2container.packages.nix2container.buildImage {
+          name = "hello";
+          config = {
+            entrypoint = [ "${pkgs.hello}/bin/hello" ];
+          };
         };
       };
     };
