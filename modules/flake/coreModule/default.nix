1835d80bf9402ba51a6bfaf884b3f53e20e8c1d3
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2026-01-08 17:11:31 -0500

    test: overlays

--- modules/flake/coreModule/default.nix
+++ modules/flake/coreModule/default.nix
@@ -1,6 +1,11 @@
+{ self, ... }:
 {
   imports = [
     ./services/ssh.nix
     ./programs/forgecode
   ];
+
+  flake.nixosModules.core = {
+    nixpkgs.overlays = [ self.overlays.default ];
+  };
 }
