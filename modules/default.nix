537847906dff6389499b05f68e77fcff1b300f47
yung-gimp <172232649+yung-gimp@users.noreply.github.com>
2026-01-23 01:54:36 +0000

    feat: fancier import
    more ways to disable module files, adding to a list is more git friendly than renaming a file to disable it.

--- modules/default.nix
+++ modules/default.nix
@@ -1,9 +1,15 @@
 { lib, ... }:
-{
-  # Import all files in this directory and subdirectories
-  # Ignores paths that contain an "_" at any level
+let
+  # Is your module broken? Add it here and forget about it!
+  excludedFiles = [ ];
 
-  imports = lib.filter (n: (!lib.hasInfix "_" (builtins.toString n) && (n != ./. + "/default.nix"))) (
-    lib.filesystem.listFilesRecursive ./.
-  );
+  # Lists all .nix files in this directory and its children
+  # Excludes this file and any files whose path contains an "_" at any level
+  moduleFiles = lib.filter (
+    n: (lib.hasSuffix ".nix" n) && (!lib.hasInfix "_" (toString n) && (n != ./default.nix))
+  ) (lib.filesystem.listFilesRecursive ./.);
+in
+{
+
+  imports = lib.subtractLists excludedFiles moduleFiles;
 }
