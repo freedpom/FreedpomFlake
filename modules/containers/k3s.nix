a6ec3e349c5df26e3c3c4e3b7f4f0d94a175ac8d
Neochaotics <72465280+Neochaotics@users.noreply.github.com>
2026-02-23 11:20:47 -0500

    fix: firewall

--- modules/containers/k3s.nix
+++ modules/containers/k3s.nix
@@ -18,9 +18,12 @@
         services.k3s = {
           enable = true;
           role = "server";
-          clusterInit = true;
           images = [ pkgs.grafana-oci ];
         };
+        networking.firewall.allowedTCPPorts = [
+          6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+         ];
+
 
         environment.systemPackages = with pkgs; [
           kubectl
